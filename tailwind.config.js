076541b7fb6fd561578f697090fe831f3908d23a
vishnu-vardhan121 <kottevishnuvardhann@gmail.com>
2025-02-18 17:12:07 +0530

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,17 @@ export default {
       columns: {
         // Adds a 5-column layout
       },
+      colors: {
+        "insta-pink": "#E1306C",
+        "insta-orange": "#FD1D1D",
+        "insta-yellow": "#FFC107",
+        "insta-purple": "#833AB4",
+        "insta-blue": "#405DE6",
+      },
+      backgroundImage: {
+        "instagram-gradient":
+          "linear-gradient(45deg, #833AB4, #FD1D1D, #FFC107, #E1306C)",
+      },
     },
   },
   plugins: [],
