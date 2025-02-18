e94f6149e57f8bbaa710276c6e1e710864421d23
vishnu-vardhan121 <kottevishnuvardhann@gmail.com>
2025-02-18 17:19:02 +0530

    gallary

--- src/components/HomeGallery.jsx
+++ src/components/HomeGallery.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { motion } from "framer-motion";
-import Gallary from "./Gallary.jsx";
+import Gallary from "./Gallary";
 
 function HomeGallery() {
   const [currentSection, setCurrentSection] = useState("kids");
