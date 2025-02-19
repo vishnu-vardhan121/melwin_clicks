0a07c1598bface1de6c727853f98eb200acf2872
vishnu-vardhan121 <kottevishnuvardhann@gmail.com>
2025-02-19 11:50:31 +0530

    emo

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "/",
   build: {
-    outDir: "dist", // Ensure the output folder is correct
+    outDir: "dist",
+  },
+  server: {
+    historyApiFallback: true, // Ensures React Router works correctly
   },
 });
